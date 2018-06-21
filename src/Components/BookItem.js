501b37e6c7bbd8eec691cad51fe1c871a9d5d9b8
LinaLtu <lsuodyte@babbel.com>
2018-06-21 16:24:22 +0200

    Added Images

--- src/Components/BookItem.js
+++ src/Components/BookItem.js
@@ -3,6 +3,8 @@ import BookShelfChanger from "./BookShelfChanger.js";
 
 class BookItem extends React.Component {
   render() {
+    console.log(this.props.bookInfo.imageLinks.thumbnail);
+    let url = this.props.bookInfo.imageLinks.thumbnail;
     return (
       <div className="book">
         <div className="book-top">
@@ -11,8 +13,7 @@ class BookItem extends React.Component {
             style={{
               width: 128,
               height: 193,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+              backgroundImage: `url(${url})`
             }}
           />
           <BookShelfChanger
