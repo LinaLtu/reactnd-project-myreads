0c9b96922fc0f5fd6ed75fe6681e9ac8c11bc957
Lina Suodyte <lsuodyte@gmail.com>
2018-06-25 21:40:23 +0200

    Added Shelf:none to Books from Search Result

--- src/Components/BookShelfChanger.js
+++ src/Components/BookShelfChanger.js
@@ -15,6 +15,7 @@ class BookShelfChanger extends React.Component {
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
                     <option value="read">Read</option>
+                    <option value="none">None</option>
                 </select>
             </div>
         );
